eae556e92f2c248498846c6384d3ecb67477f56c
jorgepineda <63015392+jorgepineda0524@users.noreply.github.com>
2021-09-04 08:40:21 -0500

    Marked entities are added

---
+++ dialingrecords.Common/Models/Marking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dialingrecords.Common.Models
+{
+    public class Marking
+    {
+        public int IdEmpleyee { get; set; }
+        public DateTime Date { get; set; }
+        public int Type { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ dialingrecords.Functions/Entities/MarkingEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace dialingrecords.Functions.Entities
+{
+    public class MarkingEntity : TableEntity
+    {
+        public int IdEmpleyee { get; set; }
+        public DateTime Date { get; set; }
+        public int Type { get; set; }
+        public bool Consolidated { get; set; }
+    }
+}
